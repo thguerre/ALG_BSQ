e2b742767167aace4f05e5f602f74c0044aa16a0
Théo Guerre <guerre.theo@gmail.com>
2022-11-06 21:33:23 +0100

    chg - rewrote tests

--- Bonus/tests_c.sh
+++ Bonus/tests_c.sh
@@ -1,12 +1,30 @@
 
 
+errors=0
+counter=1
+failedList=""
 
 for f in mouli_maps/*
 do
-    echo "$(echo $f | sed s/mouli_maps//)"
+    echo "TEST #$counter - $(echo $f | sed 's/mouli_maps\///')"
     fs=$(echo $f | sed s/mouli_maps/mouli_maps_solved/)
-    echo "$fs"
     ./bsq $f > output
-    diff output $fs
-    echo ""
+    if [[ $(cmp output $fs) ]]; then
+        echo -e "FAILED\n"
+        errors=$((errors+1))
+        failedList="$failedList #$counter - $f\n"
+    else
+        echo -e "PASSED\n"
+    fi
+
+    counter=$((counter+1))
 done
+
+rm output
+
+if [[ "$errors" -eq "0" ]]; then
+    echo "ALL TEST PASSED !"
+else
+    echo "FAILED $errors TESTS:"
+    echo -ne "$failedList"
+fi
